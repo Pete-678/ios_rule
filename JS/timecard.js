4769328144817a8e8f4f6bf2dad52598bc700d50
Pete-678 <77386199+Pete-678@users.noreply.github.com>
2022-04-30 12:13:44 +0800

    vacation reminder js

---
+++ JS/timecard.js
@@ -0,0 +1,103 @@
+var tlist = {
+  1: ["清明節", "2022-04-05"],
+  2: ["勞動節補假", "2022-05-02"],
+  3: ["端午節", "2022-06-03"],
+  4: ["中秋節補假", "2022-09-09"],
+  5: ["國慶節", "2022-10-01"],
+  6: ["元旦", "2023-01-01"],
+};
+let tnow = new Date();
+let tnowf =
+  tnow.getFullYear() + "-" + (tnow.getMonth() + 1) + "-" + tnow.getDate();
+
+/* 计算2个日期相差的天数，不包含今天，如：2016-12-13到2016-12-15，相差2天
+ * @param startDateString
+ * @param endDateString
+ * @returns
+ */
+function dateDiff(startDateString, endDateString) {
+  var separator = "-"; //日期分隔符
+  var startDates = startDateString.split(separator);
+  var endDates = endDateString.split(separator);
+  var startDate = new Date(startDates[0], startDates[1] - 1, startDates[2]);
+  var endDate = new Date(endDates[0], endDates[1] - 1, endDates[2]);
+  return parseInt(
+    (endDate - startDate) / 1000 / 60 / 60 / 24
+  ).toString();
+}
+
+//计算输入序号对应的时间与现在的天数间隔
+function tnumcount(num) {
+  let dnum = num;
+  return dateDiff(tnowf, tlist[dnum][1]);
+}
+
+//获取最接近的日期
+function now() {
+  for (var i = 1; i <= Object.getOwnPropertyNames(tlist).length; i++) {
+    if (Number(dateDiff(tnowf, tlist[i.toString()][1])) >= 0) {
+      //console.log("最近的日期是:" + tlist[i.toString()][0]);
+      //console.log("列表长度:" + Object.getOwnPropertyNames(tlist).length);
+      //console.log("时间差距:" + Number(dateDiff(tnowf, tlist[i.toString()][1])));
+      return i;
+    }
+  }
+}
+
+//如果是0天，发送emoji;
+let nowlist = now();
+function today(day) {
+  let daythis = day;
+  if (daythis == "0") {
+    datenotice();
+    return "🎉";
+  } else {
+    return daythis;
+  }
+}
+
+//提醒日当天发送通知
+function datenotice() {
+  if ($persistentStore.read("timecardpushed") != tlist[nowlist][1] && tnow.getHours() >= 6) {
+    $persistentStore.write(tlist[nowlist][1], "timecardpushed");
+    $notification.post("假日祝福","", "今天是" + tlist[nowlist][1] + "日 " + tlist[nowlist][0] + "   🎉")
+  } else if ($persistentStore.read("timecardpushed") == tlist[nowlist][1]) {
+    //console.log("当日已通知");
+  }
+}
+
+//>图标依次切换乌龟、兔子、闹钟、礼品盒
+function icon_now(num){
+  if(num<=7 && num>3 ){
+    return "hare"
+  }else if(num<=3 && num>0){
+    return "timer"
+  }else if(num==0){
+    return "gift"
+  }else{
+    return "tortoise"
+  }
+}
+
+$done({
+title:title_random(tnumcount(Number(nowlist))),
+icon:icon_now(tnumcount(Number(nowlist))),
+content:tlist[nowlist][0]+":"+today(tnumcount(nowlist))+"天,"+tlist[Number(nowlist) + Number(1)][0] +":"+ tnumcount(Number(nowlist) + Number(1))+ "天,"+tlist[Number(nowlist) + Number(2)][0]+":"+tnumcount(Number(nowlist) + Number(2))+"天"
+})
+
+function title_random(num){
+  let r = Math.floor((Math.random()*10)+1);
+  let dic = {
+    1:"距離放假，還要摸魚多少天？",
+    2:"加油，就快放假啦！",
+    3:"上班好累呀，好想放假",
+    4:"努力，我還能加班24小時！",
+    5:"天吶，還要多久才放假呀？",
+    6:"躺平中，等放假",
+    7:"只有摸魚才是賺老闆的錢",
+    8:"一起摸魚吧",
+    9:"摸魚中，期待下一個假日",
+    10: "小烏龜慢慢爬"
+  };
+  return num==0?"假日快樂，萬事如意":dic[r]
+}
